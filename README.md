ff9c038928aeb736f3743ef14b1043921ad22573
David Pederson <dpederson@figure.com>
2023-12-11 20:15:57 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# fauxstodian-gateway
+
+A http gateway for the fauxstodian gRPC server.
+
+### Generate
+
+To generate code and swagger spec
+
+```sh
+buf generate
+```
+
+### Build
+
+To build the fauxstodian gateway binary
+
+```sh
+go build
+```
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"net/http"
+
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/grpclog"
+
+	gw "github.com/carp-cobain/fauxstodian-gateway/gen/go/fauxstodian"
+)
+
+var grpcEndpoint = flag.String("grpc-endpoint", "localhost:50055", "gRPC server endpoint")
+
+func run() error {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	// Register gRPC server endpoint
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+	if err := gw.RegisterFauxstodianHandlerFromEndpoint(ctx, mux, *grpcEndpoint, opts); err != nil {
+		return err
+	}
+
+	// Start HTTP server, proxying calls to gRPC the server
+	return http.ListenAndServe(":8080", mux)
+}
+
+func main() {
+	flag.Parse()
+
+	if err := run(); err != nil {
+		grpclog.Fatal(err)
+	}
+}
